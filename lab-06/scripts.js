4b8c10c2f3fc6f0972139461f9836c36430b05ab
Jspanglez <jeevpangli@gmail.com>
2023-02-01 17:00:45 +0000

    Initial commit

---
+++ lab-06/scripts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// function addItem(text) {
+//   const item = document.createElement('li');
+//   item.textContent = text;
+//   todo.appendChild(item);
+// }
+
+function addItem(text, done) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  label.textContent = text;
+  input.type = "checkbox";
+  input.checked = done;
+  input.id = `todo${todo.querySelectorAll('li').length + 1}`;
+  label.htmlFor = input.id;
+  item.appendChild(label);
+  item.appendChild(input);
+  todo.appendChild(item);
+}
+
+add.addEventListener('click', ev => {
+  addItem(text.value);
+});
