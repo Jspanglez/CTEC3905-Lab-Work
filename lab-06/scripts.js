f40b01a4e279387870b4d82967ab20b20767fe86
Jspanglez <jeevpangli@gmail.com>
2023-02-08 16:28:40 +0000

    Added a button that deletes an item

--- lab-06/scripts.js
+++ lab-06/scripts.js
@@ -10,13 +10,21 @@ function addItem(text, done) {
   const item = document.createElement('li');
   const label = document.createElement('label');
   const input = document.createElement('input');
+  const button = document.createElement('button');
   label.textContent = text;
+  button.textContent = "x";
+
+  button.addEventListener('click', ev => {
+    item.remove();
+  });
+
   input.type = "checkbox";
   input.checked = done;
   input.id = `todo${todo.querySelectorAll('li').length + 1}`;
   label.htmlFor = input.id;
-  item.appendChild(label);
   item.appendChild(input);
+  item.appendChild(label);
+  item.appendChild(button);
   todo.appendChild(item);
 }
 
